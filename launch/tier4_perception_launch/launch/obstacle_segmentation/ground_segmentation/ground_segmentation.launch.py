f44bafdf6aa0aae5bc0c52d33013d15775cd5642
Shunsuke Miura <37187849+miursh@users.noreply.github.com>
2023-03-22 13:38:45 +0900

    feat(elevation_map_loader): use polygon iterator to speed up (#2885)
    * use grid_map::PolygonIterator instead of grid_map::GridMapIterator
    
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>
    
    * formatting
    
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>
    
    * use use_lane_filter option
    
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>
    
    * delete unused use-lane-filter option
    
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>
    
    * change use_lane_filter to True, clarify the scope
    
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>
    
    * change to use grid_map_utils::PolygonIterator
    
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>
    
    * Add lane margin parameter
    
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>
    
    * use boost geometry buffer to expand lanes
    
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>
    
    * Change use_lane_filter param default to false
    
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>
    
    * update README
    
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>
    
    ---------
    
    Signed-off-by: Shunsuke Miura <shunsuke.miura@tier4.jp>

--- launch/tier4_perception_launch/launch/obstacle_segmentation/ground_segmentation/ground_segmentation.launch.py
+++ launch/tier4_perception_launch/launch/obstacle_segmentation/ground_segmentation/ground_segmentation.launch.py
@@ -327,6 +327,7 @@ class GroundSegmentationPipeline:
                         "use_lane_filter": False,
                         "use_inpaint": True,
                         "inpaint_radius": 1.0,
+                        "lane_margin": 2.0,
                         "param_file_path": PathJoinSubstitution(
                             [
                                 LaunchConfiguration("tier4_perception_launch_param_path").perform(
