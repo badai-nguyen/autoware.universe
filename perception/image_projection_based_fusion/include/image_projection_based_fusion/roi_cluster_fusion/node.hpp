71f9cbc550bbffb9b7a461324e549e1489ca556a
badai nguyen <94814556+badai-nguyen@users.noreply.github.com>
2023-07-22 14:31:09 +0900

    fix(roi_cluster_fusion): fuse unknown roi (#4123)
    * fix: fuse and keep unknown roi
    
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>
    
    * fix: add unknown iou threshold
    
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>
    
    * fix: update existence probability for fused objects
    
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>
    
    * docs: update param
    
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>
    
    * chores: refactor
    
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>
    
    ---------
    
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>

--- perception/image_projection_based_fusion/include/image_projection_based_fusion/roi_cluster_fusion/node.hpp
+++ perception/image_projection_based_fusion/include/image_projection_based_fusion/roi_cluster_fusion/node.hpp
@@ -43,6 +43,9 @@ protected:
   bool use_iou_{false};
   bool use_cluster_semantic_type_{false};
   float iou_threshold_{0.0f};
+  float unknown_iou_threshold_{0.0f};
+  const float min_roi_existence_prob_ =
+    0.1;  // keep small value to lessen affect on merger object stage
   bool remove_unknown_;
   float trust_distance_;
 
