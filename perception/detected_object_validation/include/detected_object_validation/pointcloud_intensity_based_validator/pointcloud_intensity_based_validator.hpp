e0c57c8fbc5d2928dbfc2965a5be24aa01616076
badai-nguyen <dai.nguyen@tier4.jp>
2024-03-22 18:28:08 +0900

    fix: pointcloud intensity based validator
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>

--- perception/detected_object_validation/include/detected_object_validation/pointcloud_intensity_based_validator/pointcloud_intensity_based_validator.hpp
+++ perception/detected_object_validation/include/detected_object_validation/pointcloud_intensity_based_validator/pointcloud_intensity_based_validator.hpp
@@ -15,14 +15,16 @@
 #ifndef DETECTED_OBJECT_VALIDATION__POINTCLOUD_INTENSITY_BASED_VALIDATOR__POINTCLOUD_INTENSITY_BASED_VALIDATOR_HPP_
 #define DETECTED_OBJECT_VALIDATION__POINTCLOUD_INTENSITY_BASED_VALIDATOR__POINTCLOUD_INTENSITY_BASED_VALIDATOR_HPP_
 
-#include <rclcpp/rclcpp.hpp>
-#include <tier4_perception_msgs/msg/detected_objects_with_feature.hpp>
 #include <detected_object_validation/utils/utils.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <tier4_perception_msgs/msg/detected_objects_with_feature.hpp>
+
 #include <tf2/convert.h>
 #include <tf2/transform_datatypes.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
-#include <sensor_msgs/msg/point_cloud2.hpp>
 
 namespace pointcloud_intensity_based_validator
 {
@@ -30,18 +32,20 @@ class PointCloudIntensityBasedValidator : public rclcpp::Node
 {
 public:
   explicit PointCloudIntensityBasedValidator(const rclcpp::NodeOptions & node_options);
+
 private:
-  void onObjects(const tier4_perception_msgs::msg::DetectedObjectsWithFeature::ConstSharedPtr input_objects_msg);
+  void onObjects(
+    const tier4_perception_msgs::msg::DetectedObjectsWithFeature::ConstSharedPtr input_objects_msg);
   bool isObjectValid(const sensor_msgs::msg::PointCloud2 & object);
   rclcpp::Publisher<tier4_perception_msgs::msg::DetectedObjectsWithFeature>::SharedPtr object_pub_;
-  rclcpp::Subscription<tier4_perception_msgs::msg::DetectedObjectsWithFeature>::SharedPtr object_sub_;  
+  rclcpp::Subscription<tier4_perception_msgs::msg::DetectedObjectsWithFeature>::SharedPtr
+    object_sub_;
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
   double intensity_threshold_;
   double existance_probability_threshold_;
   utils::FilterTargetLabel filter_target_;
 };
-} //namespace pointcloud_intensity_based_validator
-
+}  // namespace pointcloud_intensity_based_validator
 
 #endif  // DETECTED_OBJECT_VALIDATION__POINTCLOUD_INTENSITY_BASED_VALIDATOR__POINTCLOUD_INTENSITY_BASED_VALIDATOR_HPP_
