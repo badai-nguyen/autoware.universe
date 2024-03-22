3e86b9f61d37256ad8a5f5fd87cb51c9d629ebe0
badai-nguyen <dai.nguyen@tier4.jp>
2024-03-22 15:36:48 +0900

    feat: add intensity based validator
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>

---
+++ perception/intensity_based_validator/include/intensity_based_validator/intensity_based_validator.hpp
@@ -0,0 +1,52 @@
+// Copyright 2022 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INTENSITY_BASED_VALIDATOR__INTENSITY_BASED_VALIDATOR_HPP_
+#define INTENSITY_BASED_VALIDATOR__INTENSITY_BASED_VALIDATOR_HPP_
+
+
+
+#include <rclcpp/rclcpp.hpp>
+#include <tier4_perception_msgs/msg/detected_objects_with_feature.hpp>
+
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+
+#include <string>
+
+namespace intensity_based_validator
+{
+
+class IntensityBasedValidator : public rclcpp::Node
+{
+public:
+  explicit IntensityBasedValidator(const rclcpp::NodeOptions & node_options);
+
+private:
+  void objectCallback(const tier4_perception_msgs::msg::DetectedObjectsWithFeature::ConstSharedPtr input_msg);
+  bool isValidatedCluster(const sensor_msgs::msg::PointCloud2 & cluster);
+
+  rclcpp::Publisher<tier4_perception_msgs::msg::DetectedObjectsWithFeature>::SharedPtr object_pub_;
+  rclcpp::Subscription<tier4_perception_msgs::msg::DetectedObjectsWithFeature>::SharedPtr object_sub_;
+
+  tf2_ros::Buffer tf_buffer_;
+  tf2_ros::TransformListener tf_listener_;
+  double intensity_threshold_;
+  double existance_probability_threshold_;
+
+};
+
+}  // namespace intensity_based_validator
+
+#endif  // INTENSITY_BASED_VALIDATOR__INTENSITY_BASED_VALIDATOR_HPP_
