31af0b92ab1a0771a16d4d86defffd5c4b011e89
badai-nguyen <dai.nguyen@tier4.jp>
2024-03-22 18:14:47 +0900

    chore: precommit intensity_based_validator
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>

--- perception/intensity_based_validator/include/intensity_based_validator/intensity_based_validator.hpp
+++ perception/intensity_based_validator/include/intensity_based_validator/intensity_based_validator.hpp
@@ -15,9 +15,8 @@
 #ifndef INTENSITY_BASED_VALIDATOR__INTENSITY_BASED_VALIDATOR_HPP_
 #define INTENSITY_BASED_VALIDATOR__INTENSITY_BASED_VALIDATOR_HPP_
 
-
-
 #include <rclcpp/rclcpp.hpp>
+
 #include <tier4_perception_msgs/msg/detected_objects_with_feature.hpp>
 
 #include <tf2_ros/buffer.h>
@@ -34,17 +33,18 @@ public:
   explicit IntensityBasedValidator(const rclcpp::NodeOptions & node_options);
 
 private:
-  void objectCallback(const tier4_perception_msgs::msg::DetectedObjectsWithFeature::ConstSharedPtr input_msg);
+  void objectCallback(
+    const tier4_perception_msgs::msg::DetectedObjectsWithFeature::ConstSharedPtr input_msg);
   bool isValidatedCluster(const sensor_msgs::msg::PointCloud2 & cluster);
 
   rclcpp::Publisher<tier4_perception_msgs::msg::DetectedObjectsWithFeature>::SharedPtr object_pub_;
-  rclcpp::Subscription<tier4_perception_msgs::msg::DetectedObjectsWithFeature>::SharedPtr object_sub_;
+  rclcpp::Subscription<tier4_perception_msgs::msg::DetectedObjectsWithFeature>::SharedPtr
+    object_sub_;
 
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
   double intensity_threshold_;
   double existance_probability_threshold_;
-
 };
 
 }  // namespace intensity_based_validator
