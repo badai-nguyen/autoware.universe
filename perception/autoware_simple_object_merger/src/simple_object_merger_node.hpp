29878394248d9bd6184250a1d3a749e9e56dce90
badai-nguyen <dai.nguyen@tier4.jp>
2025-04-16 14:05:18 +0900

    fix(simple_object_merger): add approximatefilter
    Signed-off-by: badai-nguyen <dai.nguyen@tier4.jp>

--- perception/autoware_simple_object_merger/src/simple_object_merger_node.hpp
+++ perception/autoware_simple_object_merger/src/simple_object_merger_node.hpp
@@ -20,6 +20,10 @@
 
 #include "autoware_perception_msgs/msg/detected_objects.hpp"
 
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/synchronizer.h>
+
 #include <chrono>
 #include <memory>
 #include <string>
@@ -48,6 +52,18 @@ private:
   rclcpp::Subscription<DetectedObjects>::SharedPtr sub_objects_{};
   std::vector<rclcpp::Subscription<DetectedObjects>::SharedPtr> sub_objects_array{};
   std::shared_ptr<autoware_utils::TransformListener> transform_listener_;
+
+  // Subscriber by message_filter
+  message_filters::Subscriber<DetectedObjects> input0_{};
+  message_filters::Subscriber<DetectedObjects> input1_{};
+  using SyncPolicy =
+    message_filters::sync_policies::ApproximateTime<DetectedObjects, DetectedObjects>;
+  using Sync = message_filters::Synchronizer<SyncPolicy>;
+  typename std::shared_ptr<Sync> sync_ptr_;
+
+  void approximateMerger(
+    const DetectedObjects::ConstSharedPtr & object_msg0,
+    const DetectedObjects::ConstSharedPtr & object_msg1);
 
   // Callback
   void onData(const DetectedObjects::ConstSharedPtr msg, size_t array_number);
